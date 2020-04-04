f3cf3500b7fd6d2d7eb9ed6e119cfe648bb15ba4
Tomosuke <orenoti@gmail.com>
2020-04-04 22:07:57 +0900

    Add PokemonDetailTests.swift

---
+++ DomainTests/PokemonDetail/PokemonDetailTests.swift
@@ -0,0 +1,16 @@
+//
+//  PokemonDetailTests.swift
+//  DomainTests
+//
+//  Created by Tomosuke Okada on 2020/04/04.
+//
+
+import XCTest
+@testable import DataStore
+
+final class PokemonDetailTests: XCTestCase {
+    
+    func testImagePattern() throws {
+        
+    }
+}
